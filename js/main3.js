9256324705cd931784cacbd2aeb34c68d0f0c853
Vittorio <vienianchetu@gmail.com>
2018-02-14 22:41:23 +0100

    first commit

---
+++ js/main3.js
@@ -0,0 +1,8 @@
+var myData ={
+    seen: true
+  };
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: myData
+})
---
+++ js/main4.js
@@ -0,0 +1,13 @@
+
+var myData ={
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  };
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: myData
+})
